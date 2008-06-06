bc29e8be37813b6d661f056b17f279cc3fb76fab
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2008-06-06 06:36:56 +0000

    Initial code includes functional DateTime object and an Enum-like Ordinal object (backward compatible with Java 1.4).

---
+++ trunk/src/org/pojava/validation/AlertLevel.java
@@ -0,0 +1,49 @@
+package org.pojava.validation;
+
+/*
+ Copyright 2008 John Pile
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import java.util.Iterator;
+
+import org.pojava.ordinals.Ordinal;
+import org.pojava.ordinals.OrdinalSet;
+
+public class AlertLevel extends Ordinal {
+
+	private static final OrdinalSet ordinals = new OrdinalSet();
+
+	public static final AlertLevel INFO = new AlertLevel("INFO");
+	public static final AlertLevel ERROR = new AlertLevel("ERROR");
+	public static final AlertLevel WARNING = new AlertLevel("WARNING");
+
+	private AlertLevel(String name) {
+		register(ordinals, name, this);
+	}
+
+	public Iterator iterator() {
+		return ordinals.iterator();
+	}
+
+	public static AlertLevel valueOf(String name) {
+		Ordinal located = ordinals.get(name);
+		if (located == null) {
+			throw new IllegalArgumentException("No ordinal class "
+					+ AlertLevel.class.getName() + "." + name);
+		}
+		return (AlertLevel) located;
+	}
+
+}
