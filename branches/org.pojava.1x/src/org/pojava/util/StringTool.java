6b7080eb125d8599be6dc1d4b3f81df6bbd8f1ea
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2008-10-06 07:28:36 +0000

    Naming convention conversion.

--- branches/org.pojava.1x/src/org/pojava/util/StringTool.java
+++ branches/org.pojava.1x/src/org/pojava/util/StringTool.java
@@ -1,4 +1,19 @@
 package org.pojava.util;
+/*
+Copyright 2008 John Pile
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 public class StringTool {
 
@@ -95,6 +110,48 @@ public class StringTool {
 			}
 		}
 		return isNeg ? -parsed : parsed;
+	}
+	
+	/**
+	 * Capitalize the first character of a String.
+	 * @param str
+	 * @return
+	 */
+	public static String capitalize(String str) {
+		char[] chars=str.toCharArray();
+		chars[0]=Character.toUpperCase(chars[0]);
+		return new String(chars);
+	}
+	
+	/**
+	 * Convert this_style to thisStyle.
+	 * @param str
+	 * @return
+	 */
+	public static String camelFromUnderscore(String str) {
+		if (str==null) return null;
+		StringBuffer sb=new StringBuffer();
+		char[] a=str.toCharArray();
+		boolean up=false;
+		for (int i=0; i<a.length; i++) {
+			char c=a[i];
+			if (c=='_') {
+				up=true;
+			} else {
+				if (up) {
+					up=false;
+					if (c>='0' && c<='9') {
+						sb.append('_');
+						sb.append(c);
+					} else {
+						sb.append(Character.toUpperCase(c));
+					}
+				} else {
+					sb.append(c);
+				}
+			}
+		}
+		return sb.toString();
 	}
 
 }
