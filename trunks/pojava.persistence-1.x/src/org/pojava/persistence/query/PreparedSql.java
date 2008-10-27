01fc2e4ca83a0f782df03a0352b216253a8a9e22
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2008-10-27 00:10:23 +0000

    Code formatting and copyright.

--- trunks/pojava.persistence-1.x/src/org/pojava/persistence/query/PreparedSql.java
+++ trunks/pojava.persistence-1.x/src/org/pojava/persistence/query/PreparedSql.java
@@ -1,5 +1,20 @@
 package org.pojava.persistence.query;
 
+/*
+ Copyright 2008 John Pile
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 import org.pojava.lang.BoundString;
 
 /**
@@ -19,13 +34,15 @@ public class PreparedSql {
 	 */
 	BoundString sql;
 	/**
-	 * maxRows limits the number of rows returned by the JDBC driver.
-	 * It may also be used to alter the SQL statement under certain conditions.
+	 * maxRows limits the number of rows returned by the JDBC driver. It may
+	 * also be used to alter the SQL statement under certain conditions.
 	 */
-	int maxRows = 0;	
+	int maxRows = 0;
 
 	/**
-	 * Create PreparedSql from SQL stored in a BoundString with unlimited maxRows.
+	 * Create PreparedSql from SQL stored in a BoundString with unlimited
+	 * maxRows.
+	 * 
 	 * @param sql
 	 */
 	public PreparedSql(BoundString sql) {
@@ -33,7 +50,9 @@ public class PreparedSql {
 	}
 
 	/**
-	 * Create PreparedSql from SQL stored in a BoundString with specified maxRows.
+	 * Create PreparedSql from SQL stored in a BoundString with specified
+	 * maxRows.
+	 * 
 	 * @param sql
 	 * @param maxRows
 	 */
@@ -44,6 +63,7 @@ public class PreparedSql {
 
 	/**
 	 * Get SQL.
+	 * 
 	 * @return
 	 */
 	public BoundString getSql() {
@@ -52,6 +72,7 @@ public class PreparedSql {
 
 	/**
 	 * Get Max number of rows returnable by query.
+	 * 
 	 * @return
 	 */
 	public int getMaxRows() {
