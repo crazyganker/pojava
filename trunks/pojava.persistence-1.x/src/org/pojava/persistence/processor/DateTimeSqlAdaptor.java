0a7cfe20f44f746989b250029a15e0713077cc5a
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2008-10-27 00:09:38 +0000

    Adaptors for java.sql.Date target.

---
+++ trunks/pojava.persistence-1.x/src/org/pojava/persistence/processor/DateTimeSqlAdaptor.java
@@ -0,0 +1,46 @@
+package org.pojava.persistence.processor;
+
+import org.pojava.datetime.DateTime;
+import org.pojava.lang.Binding;
+import org.pojava.transformation.BindingAdaptor;
+
+/**
+ * Adaptor for managing Java to JDBC for a java.sql.Date value mapped to a
+ * DateTime value.
+ * 
+ * @author John Pile
+ * 
+ */
+public class DateTimeSqlAdaptor implements BindingAdaptor {
+
+	/**
+	 * Translate the binding from the data source towards Java bean.
+	 */
+	public Binding inbound(Binding inBinding) {
+		// Prevent constructing a new object when you can.
+		if (inBinding == null)
+			return null;
+		if (inBinding.getObj() == null) {
+			return inBinding;
+		}
+		Binding outBinding;
+		outBinding = new Binding(DateTime.class,
+				new DateTime(((java.util.Date) inBinding.getObj()).getTime()));
+		return outBinding;
+	}
+
+	/**
+	 * Translate the binding from the java bean to the data source.
+	 */
+	public Binding outbound(Binding outBinding) {
+		// Prevent constructing a new object when you can.
+		if (outBinding == null)
+			return null;
+		if (outBinding.getObj() == null) {
+			return outBinding;
+		}
+		Binding inBinding = new Binding(java.sql.Date.class,
+				new java.sql.Date(((DateTime) outBinding.getObj()).getMillis()));
+		return inBinding;
+	}
+}
---
+++ trunks/pojava.persistence-1.x/src/org/pojava/persistence/processor/UtilDateSqlAdaptor.java
@@ -0,0 +1,47 @@
+package org.pojava.persistence.processor;
+
+import org.pojava.lang.Binding;
+import org.pojava.transformation.BindingAdaptor;
+
+/**
+ * Adaptor for managing Java to JDBC for a java.sql.Date value mapped to a
+ * java.util.Date value or one of its derivatives.
+ * 
+ * @author John Pile
+ * 
+ */
+public class UtilDateSqlAdaptor implements BindingAdaptor {
+
+	/**
+	 * Translate the binding from the data source towards Java bean.
+	 */
+	public Binding inbound(Binding inBinding) {
+		// Prevent constructing a new object when you can.
+		if (inBinding == null)
+			return null;
+		if (inBinding.getObj() == null
+				|| java.sql.Date.class.equals(inBinding.getType())) {
+			return inBinding;
+		}
+		Binding outBinding;
+		outBinding = new Binding(java.util.Date.class,
+				(java.util.Date) inBinding.getObj());
+		return outBinding;
+	}
+
+	/**
+	 * Translate the binding from the java bean to the data source.
+	 */
+	public Binding outbound(Binding outBinding) {
+		// Prevent constructing a new object when you can.
+		if (outBinding == null)
+			return null;
+		if (outBinding.getObj() == null
+				|| java.sql.Date.class.equals(outBinding.getType())) {
+			return outBinding;
+		}
+		Binding inBinding = new Binding(java.sql.Date.class,
+				new java.sql.Date(((java.util.Date) outBinding.getObj()).getTime()));
+		return inBinding;
+	}
+}
