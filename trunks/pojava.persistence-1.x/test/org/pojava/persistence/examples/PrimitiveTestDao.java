2be3c08472705c73545b801990f9c54e0b02ed3a
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2008-11-07 06:34:33 +0000

    Closing connections in case a ConnectionSource is used that doesn't close it for you.

--- trunks/pojava.persistence-1.x/test/org/pojava/persistence/examples/PrimitiveTestDao.java
+++ trunks/pojava.persistence-1.x/test/org/pojava/persistence/examples/PrimitiveTestDao.java
@@ -1,12 +1,13 @@
 package org.pojava.persistence.examples;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 import org.pojava.lang.Processor;
 import org.pojava.persistence.query.SqlQuery;
-import org.pojava.persistence.sql.DatabaseCache;
 import org.pojava.persistence.sql.ConnectionSource;
+import org.pojava.persistence.sql.DatabaseCache;
 import org.pojava.persistence.sql.TableMap;
 import org.pojava.persistence.util.DaoTool;
 
@@ -26,58 +27,118 @@ public class PrimitiveTestDao {
 
 	public static int insert(ConnectionSource connector, PrimitiveTest obj)
 			throws SQLException {
-		return DaoTool.insert(connector.getConnection(DS_NAME), MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.insert(conn, MAP, obj);
+		} finally {
+			conn.close();
+		}
+	}
+
+	public static int[] batchInsert(ConnectionSource connector, List list)
+			throws SQLException {
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.batchInsert(connector.getConnection(DS_NAME), MAP, list);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int update(ConnectionSource connector, PrimitiveTest obj)
 			throws SQLException {
-		return DaoTool.update(connector.getConnection(DS_NAME), MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.update(connector.getConnection(DS_NAME), MAP, obj);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int updateInsert(ConnectionSource connector, PrimitiveTest obj)
 			throws SQLException {
-		return DaoTool.updateInsert(connector.getConnection(DS_NAME), MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.updateInsert(connector.getConnection(DS_NAME), MAP, obj);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int passiveInsert(ConnectionSource connector,
 			PrimitiveTest obj) throws SQLException {
-		return DaoTool
-				.passiveInsert(connector.getConnection(DS_NAME), MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool
+			.passiveInsert(connector.getConnection(DS_NAME), MAP, obj);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int delete(ConnectionSource connector, PrimitiveTest obj)
 			throws SQLException {
-		return DaoTool.delete(connector.getConnection(DS_NAME), MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.delete(connector.getConnection(DS_NAME), MAP, obj);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static PrimitiveTest find(ConnectionSource connector,
 			PrimitiveTest obj) throws SQLException {
-		return (PrimitiveTest) DaoTool.find(connector.getConnection(DS_NAME),
-				MAP, obj);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return (PrimitiveTest) DaoTool.find(connector.getConnection(DS_NAME),
+					MAP, obj);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static List listByQuery(ConnectionSource connector, SqlQuery query)
 			throws SQLException {
-		return DaoTool
-				.listByQuery(connector.getConnection(DS_NAME), MAP, query);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool
+			.listByQuery(connector.getConnection(DS_NAME), MAP, query);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int deleteByQuery(ConnectionSource connector, SqlQuery query)
 			throws SQLException {
-		return DaoTool.deleteByQuery(connector.getConnection(DS_NAME), MAP,
-				query);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.deleteByQuery(connector.getConnection(DS_NAME), MAP,
+					query);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int countByQuery(ConnectionSource connector, SqlQuery query)
 			throws SQLException {
-		return DaoTool.countByQuery(connector.getConnection(DS_NAME), MAP,
-				query);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.countByQuery(connector.getConnection(DS_NAME), MAP,
+					query);
+		} finally {
+			conn.close();
+		}
 	}
 
 	public static int processByQuery(ConnectionSource connector,
 			SqlQuery query, Processor objProcessor) throws SQLException {
-		return DaoTool.processByQuery(connector.getConnection(DS_NAME), MAP,
-				query, objProcessor);
+		Connection conn=connector.getConnection(DS_NAME);
+		try {
+			return DaoTool.processByQuery(connector.getConnection(DS_NAME), MAP,
+					query, objProcessor);
+		} finally {
+			conn.close();
+		}
 	}
 
 }
