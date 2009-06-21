c7907f9f533a37c4f7e6d4612d56339bca4cf2b9
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2009-06-21 15:50:38 +0000

    * DefaultFactory: Converted new Character to Character.valueOf

--- branches/pojava.persistence-1.4.2/src/org/pojava/persistence/factories/DefaultFactory.java
+++ branches/pojava.persistence-1.4.2/src/org/pojava/persistence/factories/DefaultFactory.java
@@ -51,7 +51,7 @@ public class DefaultFactory implements SerialFactory {
 				return new Long(value);
 			} else if (type == char.class) {
 				if (value.length() > 0) {
-					return new Character(value.charAt(0));
+					return Character.valueOf(value.charAt(0));
 				}
 			} else if (type == byte.class) {
 				if (value.length() == 2) {
