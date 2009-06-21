3175a1870b87441b0b59cebd7f6ae0a85c112825
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2009-06-21 20:16:33 +0000

    Added JavaDoc content.

--- branches/pojava.persistence-2.0.x/src/org/pojava/persistence/sql/ConnectionSource.java
+++ branches/pojava.persistence-2.0.x/src/org/pojava/persistence/sql/ConnectionSource.java
@@ -2,6 +2,13 @@ package org.pojava.persistence.sql;
 
 import java.sql.Connection;
 
+/**
+ * A connection source provides an interface allowing one to request
+ * a connection from a repository by a name.
+ * 
+ * @author John Pile
+ *
+ */
 public interface ConnectionSource {
 
     /**
