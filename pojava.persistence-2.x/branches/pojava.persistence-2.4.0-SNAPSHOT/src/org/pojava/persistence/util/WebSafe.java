2f5c5dfe3f32480eef9fa6cd686ac0b65c4ed099
john@pile.us <john@pile.us@467501ef-c34c-0410-9d6a-5d462bdaa36e>
2010-09-19 05:59:10 +0000

    Release Notes: version 2.5.0 * WebSafe: Converted from Encryption (deprecated) to EncryptionTool. * WebSafe: Added optional IvParameterSpec parameter. * WebSafe: Deprecated SecretKey parameter in favor of keyString (see EncryptionTool). * pom.xml: Update pojava dependency to 2.5.0. * pom.xml: Pinned specific versions to several plugins. * Misc: Muted or appeased several generics warnings in source code.

--- pojava.persistence-2.x/branches/pojava.persistence-2.4.0-SNAPSHOT/src/org/pojava/persistence/util/WebSafe.java
+++ pojava.persistence-2.x/branches/pojava.persistence-2.4.0-SNAPSHOT/src/org/pojava/persistence/util/WebSafe.java
@@ -1,15 +1,17 @@
 package org.pojava.persistence.util;
 
+import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.util.zip.DataFormatException;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 import org.pojava.persistence.serial.XmlParser;
 import org.pojava.persistence.serial.XmlSerializer;
 import org.pojava.util.Compression;
 import org.pojava.util.EncodingTool;
-import org.pojava.util.Encryption;
+import org.pojava.util.EncryptionTool;
 
 /**
  * WebSafe is a utility for transforming objects into web-safe strings that
@@ -21,36 +23,75 @@ import org.pojava.util.Encryption;
  */
 public class WebSafe {
 
-    private Encryption crypto;
     private Compression zipper;
     private XmlParser<Object> parser;
     private XmlSerializer serial;
+    private String key;
 
-    public WebSafe(String key) throws InvalidKeyException {
-        crypto=new Encryption(key);
+    /**
+     * Construct a web-safe with a keyString (see EncryptionTool) 
+     * @param key
+     * @throws InvalidKeyException
+     */
+    public WebSafe(String keyString) throws InvalidKeyException {
+        this.key=keyString;
         zipper=new Compression();
         parser=new XmlParser<Object>();
         serial=new XmlSerializer();
     }
 
+    /**
+     * Use an externally constructed SecretKey
+     * @param key
+     * @throws InvalidKeyException
+     * @deprecated in favor of the String parameter
+     */
     public WebSafe(SecretKey key) throws InvalidKeyException {
-        crypto=new Encryption(key);
+        this.key=key.getAlgorithm() + " " + EncodingTool.base64Encode(key.getEncoded());
         zipper=new Compression();
         parser=new XmlParser<Object>();
         serial=new XmlSerializer();
     }
     
-    Object stringToObject(String webSafeString) throws DataFormatException {
+    /**
+     * Convert a web-safe string back to its original object
+     * @param webSafeString
+     * @return
+     * @throws DataFormatException
+     * @throws GeneralSecurityException
+     */
+    Object stringToObject(String webSafeString) throws DataFormatException, GeneralSecurityException {
         byte[] encrypted=EncodingTool.base64Decode(webSafeString.toCharArray());
-        byte[] zipped=crypto.decrypt(encrypted);
+        byte[] zipped=EncryptionTool.decrypt(encrypted, key);
         String xml=new String(zipper.decompress(zipped));
         return parser.parse(xml);
     }
     
-    String objectToString(Object obj) {
+    /**
+     * Convert a web-safe string back to its original object
+     * @param webSafeString
+     * @return
+     * @throws DataFormatException
+     * @throws GeneralSecurityException
+     */
+    Object stringToObject(String webSafeString, IvParameterSpec ivps) throws DataFormatException, GeneralSecurityException {
+        byte[] encrypted=EncodingTool.base64Decode(webSafeString.toCharArray());
+        byte[] zipped=EncryptionTool.decrypt(encrypted, key, ivps);
+        String xml=new String(zipper.decompress(zipped));
+        return parser.parse(xml);
+    }
+    
+    /**
+     * Convert an object to a web-safe string
+     * @param obj
+     * @param ivps IV or salt to obfuscate repeated instances of encrypted data
+     * @return
+     * @throws GeneralSecurityException
+     */
+    String objectToString(Object obj) throws GeneralSecurityException {
         String xml=serial.toXml(obj);
         byte[] zipped=zipper.compress(xml.getBytes());
-        byte[] encrypted=crypto.encrypt(zipped);
+        byte[] encrypted=EncryptionTool.encrypt(zipped, key);
         StringBuilder encoded=new StringBuilder(new String(EncodingTool.base64Encode(encrypted)));
         int len=encoded.length();
         // Base64 pads with '=', but Tomcat 6.0.14+ doesn't like '=' in cookie values.
@@ -62,4 +103,25 @@ public class WebSafe {
         return encoded.toString();
     }
     
+    /**
+     * Convert an object to a web-safe string
+     * @param obj
+     * @param ivps IV or salt to obfuscate repeated instances of encrypted data
+     * @return
+     * @throws GeneralSecurityException
+     */
+    String objectToString(Object obj, IvParameterSpec ivps) throws GeneralSecurityException {
+        String xml=serial.toXml(obj);
+        byte[] zipped=zipper.compress(xml.getBytes());
+        byte[] encrypted=EncryptionTool.encrypt(zipped, key, ivps);
+        StringBuilder encoded=new StringBuilder(new String(EncodingTool.base64Encode(encrypted)));
+        int len=encoded.length();
+        // Base64 pads with '=', but Tomcat 6.0.14+ doesn't like '=' in cookie values.
+        // I haven't figured out what paragraph of rfc2109 or rfc2965 is driving that interpretation.
+        // Since they are easily derived, we'll just drop the '=' characters.
+        while (--len>0 && encoded.charAt(len)=='=') {
+            encoded.setLength(len);
+        }
+        return encoded.toString();
+    }
 }
